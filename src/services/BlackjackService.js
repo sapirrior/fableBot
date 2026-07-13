be09291de7be5b199aacee71a7a2d6666727bec4
sapirrior <246715373+sapirrior@users.noreply.github.com>
2026-07-13 12:44:23 +0530

    chore: remove prohibited markdown formatting from blackjack outcomes

--- src/services/BlackjackService.js
+++ src/services/BlackjackService.js
@@ -189,9 +189,9 @@ function scoreStr(hand, hide = false) {
 }
 
 const OUTCOME_LINE = {
-  bj:   (d, fmt, cur) => `🎲 - You won **+${cur} ${fmt(d)}**!`,
-  win:  (d, fmt, cur) => `🎲 - You won **+${cur} ${fmt(d)}**!`,
-  lose: (d, fmt, cur) => `🎲 - You lost **−${cur} ${fmt(Math.abs(d))}**!`,
+  bj:   (d, fmt, cur) => `🎲 - You won +${cur} ${fmt(d)}!`,
+  win:  (d, fmt, cur) => `🎲 - You won +${cur} ${fmt(d)}!`,
+  lose: (d, fmt, cur) => `🎲 - You lost −${cur} ${fmt(Math.abs(d))}!`,
   // eslint-disable-next-line no-unused-vars
   tie:  (_d, _fmt, _cur) => `🎲 - You tied!`,
   // eslint-disable-next-line no-unused-vars
